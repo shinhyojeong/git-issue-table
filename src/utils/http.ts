37dc5d4d58fc41bf4671cefe0263d953a9c9cb20
shinhyojeong <hyojeonge0@naver.com>
2023-12-28 23:17:59 +0900

    config: axios 설치 및 구성

---
+++ src/utils/http.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosHeaders } from 'axios'
+
+import type { AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+
+const BASE_URL = 'https://api.github.com/repos/facebook/react'
+
+const Axios = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'content-type': 'application/json'
+  }
+})
+
+const getResult = <T>(response: AxiosResponse<T>): T => response.data
+
+export const http = {
+  get: <Request = unknown, Response = unknown>(
+    url: string,
+    params?: Request
+  ): Promise<Response> =>
+    Axios.get<Response, AxiosResponse<Response>, Request>(url, {
+      params
+    }).then(getResult)
+}
